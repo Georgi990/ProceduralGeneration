59c766bb69827720e7b0d189e5d0336ee557966c
Kristián Kovalský <k.kovalsky@me.com>
2018-11-26 14:02:43 +0100

    MagnusScript
    added a lot of cool stuff

---
+++ Assets/Scripts/MagnusScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MagnusScript : MonoBehaviour {
+
+    public Texture2D map;
+    public static float[,] ganmap;
+    Color[] getempixels;
+
+	// Use this for initialization
+	void Awake () {
+        getempixels = map.GetPixels();
+        for (int x = 0; x < map.width; x++)
+        {
+            for (int y = 0; y < map.height; y++)
+            {
+                ganmap[x,y] = getempixels[x + y * x].r;
+            }
+        }
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -4,12 +4,13 @@ using UnityEngine;
 
 public class MapGenerator : MonoBehaviour
 {
-
+    public bool usingGANS = true;
     public enum DrawMode { NoiseMap, ColourMap, Mesh };
     public DrawMode drawMode;
 
-    public int mapWidth;
-    public int mapHeight;
+    const int mapChunkSize = 241;
+    [Range(0,6)]
+    public int levelOfDetail;
     public float noiseScale;
 
     public int octaves;
@@ -20,18 +21,34 @@ public class MapGenerator : MonoBehaviour
     public int seed;
     public Vector2 offset;
 
+    public float meshHeightMultiplier;
+    public AnimationCurve meshHeightCurve; 
+
     public bool autoUpdate;
 
     public TerrainType[] regions;
+    public void Start()
+    {
+        GenerateMap();
 
+    }
     public void GenerateMap()
     {
-        float[,] noiseMap = Noise.GenerateNoiseMap(mapWidth, mapHeight, seed, noiseScale, octaves, persistance, lacunarity, offset);
+        float[,] noiseMap;
 
-        Color[] colourMap = new Color[mapWidth * mapHeight];
-        for (int y = 0; y < mapHeight; y++)
+        if (usingGANS)
         {
-            for (int x = 0; x < mapWidth; x++)
+            noiseMap = MagnusScript.ganmap;
+        }
+        else
+        {
+            noiseMap = Noise.GenerateNoiseMap(mapChunkSize, mapChunkSize, seed, noiseScale, octaves, persistance, lacunarity, offset);
+        }
+
+        Color[] colourMap = new Color[mapChunkSize * mapChunkSize];
+        for (int y = 0; y < mapChunkSize; y++)
+        {
+            for (int x = 0; x < mapChunkSize; x++)
             {
                 float currentHeight = noiseMap[x, y];
 
@@ -39,7 +56,7 @@ public class MapGenerator : MonoBehaviour
                 {
                     if (currentHeight <= regions[i].height)
                     {
-                        colourMap[y * mapWidth + x] = regions[i].colour;
+                        colourMap[y * mapChunkSize + x] = regions[i].colour;
                         break;
                     }
                 }
@@ -52,23 +69,15 @@ public class MapGenerator : MonoBehaviour
         }
         else if (drawMode == DrawMode.ColourMap)
         {
-            display.DrawTexture(TextureGenerator.TextureFromColourMap(colourMap, mapWidth, mapHeight));
+            display.DrawTexture(TextureGenerator.TextureFromColourMap(colourMap, mapChunkSize, mapChunkSize));
         }
         else if (drawMode == DrawMode.Mesh) {
-            display.DrawMesh(MeshGenerator.GenerateTerrainMesh(noiseMap), TextureGenerator.TextureFromColourMap(colourMap, mapWidth, mapHeight));
+            display.DrawMesh(MeshGenerator.GenerateTerrainMesh(noiseMap, meshHeightMultiplier, meshHeightCurve, levelOfDetail), TextureGenerator.TextureFromColourMap(colourMap, mapChunkSize, mapChunkSize));
         }
     }
 
         void OnValidate()
         {
-            if (mapWidth < 0)
-            {
-                mapWidth = 1;
-            }
-            if (mapHeight < 0)
-            {
-                mapHeight = 1;
-            }
             if (lacunarity < 0)
             {
                 lacunarity = 1;
